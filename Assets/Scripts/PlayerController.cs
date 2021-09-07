7860e7f21f5934da13889161b3910d5e649915e1
Esbelin Antoine <ant.esbelin@gmail.com>
2021-09-07 16:15:03 +0100

    Modification système input + début PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
 public class PlayerController : MonoBehaviour
 {
 
     public float speed = 7f;
 
+    [SerializeField] private int playerID = 0;
+    [SerializeField] private Player player;
+
+    private void Start()
+    {
+        player = ReInput.players.GetPlayer(playerID);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        Vector3 movement = new Vector3(player.GetAxis("HorizontalMove"), player.GetAxis("VerticalMove"), 0f);
         transform.position += movement * Time.deltaTime * speed;
+
     }
 }
