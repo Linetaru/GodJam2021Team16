86e8277ed55453ee1dd9860226486fd5993b1ea8
Orilexx1 <teo.martin777@gmail.com>
2021-09-10 10:21:53 +0200

    Changed some options in main scene + cycle counter

--- Assets/CameraShake.cs
+++ Assets/CameraShake.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class CameraShake : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public IEnumerator Shake (float duration, float magnitude)
     {
-        
+        Vector2 originalPos = transform.localPosition;
+
+        float elapsed = 0.0f;
+
+        while(elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+
+            transform.localPosition = new Vector2(x, y);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localPosition = originalPos;
     }
 
     // Update is called once per frame
